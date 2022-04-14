cd1a30c5119c6cc8817650ad238873b28f48e10d
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-04-14 19:48:30 -0300

    update flickr credencials

---
+++ script/app.js
@@ -0,0 +1,6 @@
+(function(){
+  let flickrKeys = 'a79dbdd1d24bbdf97f202f74ff0b63ec'
+  let flickrBaseURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key='
+  let suffixURL = 'q'
+
+})
